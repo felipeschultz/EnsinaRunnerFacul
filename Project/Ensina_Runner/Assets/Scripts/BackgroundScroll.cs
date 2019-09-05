fe728881d86f400f7e03f22ba4db819c5b21060c
PC <PC@DESKTOP-FMJTUSD>
2019-09-04 23:18:46 -0300

    Add Project Unity

---
+++ Project/Ensina_Runner/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    private Material material;
+    private Vector2 offset;
+
+    public int xVelocity,
+               yVelocity;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector2(xVelocity, yVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
---
+++ Project/Ensina_Runner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float JumpForce;
+
+    private Rigidbody2D MyRigidbody;
+
+    public bool Grounded;
+    public LayerMask WhatIsGround;
+
+    private Collider2D MyCollider;
+
+    private Animator MyAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyRigidbody = GetComponent<Rigidbody2D>();
+
+        MyCollider = GetComponent<Collider2D>();
+
+        MyAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Grounded = Physics2D.IsTouchingLayers(MyCollider, WhatIsGround);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (Grounded)
+            {
+                MyRigidbody.velocity = new Vector2(MyRigidbody.velocity.x, JumpForce);
+            }
+        }
+
+        MyAnimator.SetBool("Grounded", Grounded);
+    }
+}

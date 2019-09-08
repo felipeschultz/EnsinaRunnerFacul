1cfdefea7a140ead3e6514dd5faf1b5621358e89
Felipe Schultz Ribeiro <felipeschultzribeiro2014@hotmail.com>
2019-09-08 12:16:16 -0300

    Add Text "Respostas Corretas" And "Distância"

---
+++ Project/Ensina_Runner/Assets/Scripts/DistanceManager.cs
@@ -0,0 +1,40 @@
+﻿#region Explicação da Classe
+/*
+    Classe criada para fazer o gerenciamento dos pontos do jogador, tanto os pontos de "Distância" como os pontos de "Respostas Corretas".
+*/
+#endregion
+
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceManager : MonoBehaviour
+{
+    // Instância para Manipular o Texto RespCorrectText da UI.
+    public Text RespCorrectText;
+
+    // Instância para Manipular o Texto DistanciaText da UI.
+    public Text DistanceText;
+
+    public int RespCorrectCount;
+    public float DistanceCount;
+
+    public float PointsPerSeconds;
+
+    public bool DistanceIncreasing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DistanceCount += PointsPerSeconds * Time.deltaTime;
+    
+        DistanceText.text = "Distância: " + Math.Round(DistanceCount);
+        RespCorrectText.text = "Respostas Corretas: " + RespCorrectCount;
+    }
+}

0d5a2481afede210bd14afddc499807bd29ed2f1
Felipe Schultz Ribeiro <felipeschultzribeiro2014@hotmail.com>
2019-09-09 23:01:00 -0300

    Add Method and Apk - Menu

---
+++ Project/Ensina_Runner/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿#region Explicação da Classe
+/*
+ * Classe criada para a cena de menu principal, Está classe tem os métodos dos botões <Start>, <Ranking>, <Sair>.
+*/
+#endregion
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string StartGameLevel;
+
+    // Método para o Botão <Start>
+    public void StartGame()
+    {
+        SceneManager.LoadScene(StartGameLevel);
+    }
+
+    /* Método <Ranking> Não Desenvolvido
+    // Função para o Botão <Ranking>
+    public void RankingGame()
+    {
+
+    }
+    */
+
+    // Método para o Botão <Sair>
+    public void SairGame()
+    {
+        Application.Quit();
+    }
+}
